81c7770ce7e69acfec7ff93fb0d4921f11e93fab
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2024-11-27 16:50:41 +0100

    poiujhgf
    jump en cours

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,18 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float movementSpeed;
     public float jumpSpeed;
-    float horizontalInput;
-    float verticalInput;
+    private float ySpeed;
+    private float horizontalInput;
+    private float verticalInput;
+    private CharacterController conn;
+    public bool isGrounded;
+
     Vector3 moveDirection;
+    Vector3 vel;
 
     private void Start()
     {
-    
+        conn = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -32,6 +37,28 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = new Vector3(horizontalInput, 0, verticalInput);
         moveDirection.Normalize();
-        transform.Translate(moveDirection * movementSpeed);
+        //transform.Translate(moveDirection * movementSpeed);
+        conn.SimpleMove(moveDirection * movementSpeed);
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        if(Input.GetButtonDown("Jump"))
+        {
+            ySpeed = -0.5f;
+            isGrounded = false;
+        }
+        vel = moveDirection;
+        vel.y = ySpeed;
+        //transform.Translate(vel);
+        conn.SimpleMove(vel);
+        if(conn.isGrounded)
+        {
+            ySpeed = -0.5f;
+            isGrounded = true;
+            if(Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+                isGrounded = false;
+            }
+
+        }
     }
 }
