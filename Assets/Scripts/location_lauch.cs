a9245082b37cf6c03fc394baa4c8abbe63125c79
AxelPore <axel.pore@ynov.com>
2025-01-22 11:18:17 +0100

    zone_done

--- Assets/Scripts/location_lauch.cs
+++ Assets/Scripts/location_lauch.cs
@@ -6,23 +6,37 @@ public class location_lauch : MonoBehaviour
 {
     GameObject newTriggerObject;
     Rigidbody m_Rigidbody;
+    public GameObject Sphere;
+    public bool isinzone = false;
 
     void OnTriggerEnter(Collider triggerObject)
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        Debug.Log(triggerObject.name);
         if (triggerObject.name == "Zone")
     {
-        m_Rigidbody.constraints = RigidbodyConstraints.FreezePosition;
-        Debug.Log("Animation Launched");
-        GameObject.Find("decor").GetComponent<animation_test>().anime();
-
+        Instantiate(Sphere, new Vector3(10,1,5), Quaternion.identity);
+        isinzone = true;
+    } else if (triggerObject.name != "Zone")
+    {
+        Debug.Log("Exit");
+        isinzone = false;
     }
     }
-
-    void OnTriggerExit(Collider triggerObject){
-        if (Input.GetKeyDown(KeyCode.E)){
-            m_Rigidbody.constraints = RigidbodyConstraints.None;
+    void Update(){
+        Debug.Log(isinzone);
+        if (isinzone == true) {
+            if (Input.GetButtonDown("Fire1")){
+                Debug.Log("Exit");
+                m_Rigidbody.constraints = RigidbodyConstraints.None;
+            }
+            if (Input.GetButtonDown ("Fire2")){
+                Debug.Log("Enter");
+                m_Rigidbody.constraints = RigidbodyConstraints.FreezePosition;
+                Debug.Log("Animation Launched");
+                GameObject.Find("decor").GetComponent<animation_test>().anime();
+            }
         }
-            
     }
+
 }
