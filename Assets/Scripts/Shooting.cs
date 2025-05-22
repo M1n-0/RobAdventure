6a6fb941b4167ae1c99d4e647705f79a68a77b8c
Lucas Vellin-Patché <lucvellin@gmail.com>
2025-05-22 17:56:27 +0200

    tobor shooting done, rob anim work in progress

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
     public GameObject projectile;
+    public float interval;
+    public float range;
+    private float timer = 0f;
 
     void Start()
     {
-        StartCoroutine(Shoot());
-    }
+        
+    } 
 
-    IEnumerator Shoot(){
-        while(true){
-            GameObject laser = Instantiate(projectile);
-            laser.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        yield return new WaitForSeconds(Random.Range(0.75f, 1.75f));
+    void Update()
+    {
+        timer += Time.deltaTime;
+        RaycastHit hit;
+        Ray shootingRay = new Ray(transform.position, new Vector3(-1, 0, 0));
+        if (Physics.Raycast(shootingRay, out hit, range) && (timer >= interval))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                GameObject laser = Instantiate(projectile);
+                laser.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+                timer = 0f;
+            }
         }
     }
 }
