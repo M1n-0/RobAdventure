219f886de28d25a4c1f7c9c5990ca5a74c515557
NepNath <neptonath@gmail.com>
2026-05-12 16:40:18 +0200

    0.0.0 - Player Movement
    - fixed inputs for ModulX controller

--- Assets/Scirpts/JetPackScripts/PlayerMovement.cs
+++ Assets/Scirpts/JetPackScripts/PlayerMovement.cs
@@ -98,12 +98,12 @@ public class PlayerMovement : MonoBehaviour
 
     void jump()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton3) || Input.GetKeyDown(KeyCode) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) || Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             animate.SetBool("Jumping", true);
             Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
-        else if ((Input.GetKey(KeyCode.JoystickButton3) || Input.GetKey(KeyCode)) && !isGrounded())
+        else if ((Input.GetKey(KeyCode.JoystickButton3) || Input.GetKey(KeyCode.Space)) && !isGrounded())
         {
             Debug.Log("Using Jetpack");
             animate.SetBool("Jetpacking", true);
