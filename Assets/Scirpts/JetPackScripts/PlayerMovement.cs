a3adb70a89aa9da11aa0b9c6952a5cb2156c38c9
NepNath <115375139+NepNath@users.noreply.github.com>
2026-05-12 15:38:13 +0200

    0.0.0 - input manager
    - fixed input for ModulX controller

--- Assets/Scirpts/JetPackScripts/PlayerMovement.cs
+++ Assets/Scirpts/JetPackScripts/PlayerMovement.cs
@@ -98,12 +98,12 @@ public class PlayerMovement : MonoBehaviour
 
     void jump()
     {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) || KeyCode.Joystick2Button3) || Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             animate.SetBool("Jumping", true);
             Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
-        else if ((Input.GetKey(KeyCode.Joystick1Button3) || Input.GetKey(KeyCode.Space)) && !isGrounded())
+        else if ((Input.GetKey(KeyCode.JoystickButton3) || KeyCode.Joystick2Button3) || Input.GetKey(KeyCode.Space)) && !isGrounded())
         {
             Debug.Log("Using Jetpack");
             animate.SetBool("Jetpacking", true);
